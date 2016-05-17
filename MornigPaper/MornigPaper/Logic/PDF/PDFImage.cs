d64b7dba8d43541c996caf981d954e82d5747319
pi14.medvediev.roman <medvedev.roman.1995@gmail.com>
2016-05-17 14:29:37 +0300

    Added classes representing text and image PDF elements.

---
+++ MornigPaper/MornigPaper/Logic/PDF/PDFImage.cs
@@ -0,0 +1,71 @@
+﻿using HtmlAgilityPack;
+using iTextSharp.text;
+using MornigPaper.Data.HTML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MornigPaper.Logic.PDF
+{
+    /// <summary>
+    /// Represents an image.
+    /// </summary>
+    class PDFImage: IArticleElement
+    {
+        /*
+        public int Width { get; set; }
+        public int Height { get; set; }
+        */
+
+        public Image Content { get; set; }
+
+        public Uri Source { get; set; }
+
+        public PDFImage(HtmlNode node, String baseUrl)
+        {
+            String src = node.Attributes["src"].Value;
+            this.Source = new Uri(baseUrl + src);
+            this.Content = Image.GetInstance(this.Source);
+        }
+
+        public PDFImage(HtmlNode node)
+        {
+            try
+            {
+                String src = node.Attributes["src"].Value;
+                this.Source = new Uri(src);
+                this.Content = Image.GetInstance(this.Source);
+            }
+            catch(HtmlWebException e)
+            {
+                this.Content = Image.GetInstance(new Uri(@"http://www.westliguria.net/wp-content/uploads/2013/01/image-not-found.jpg")); ; 
+            }
+            
+        }
+
+        /// <summary>
+        /// Adds an image created from this element to the target document.
+        /// </summary>
+        /// <param name="pdf"> A document to add to.</param>
+        public void addToPdf(Document pdf)
+        {
+            if(!pdf.IsOpen())
+            {
+                pdf.Open();
+            }
+
+            pdf.Add(Image.GetInstance(this.Content));
+        }
+
+        /// <summary>
+        /// Scales an image to a given percentage.
+        /// </summary>
+        /// <param name="percent">New dimensions in percents</param>
+        public void Scale(float percent)
+        {
+            this.Content.ScalePercent(percent);
+        }
+    }
+}
---
+++ MornigPaper/MornigPaper/Logic/PDF/PDFText.cs
@@ -0,0 +1,93 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp;
+using iTextSharp.text;
+using MornigPaper.Data.HTML;
+
+namespace MornigPaper.Logic.PDF
+{
+    class PDFParagraph
+    {
+        //public 
+    }
+    /// <summary>
+    /// Represents a type of a text element e.g. a phrase or a paragraph.
+    /// </summary>
+    enum ElementType { Phrase, Paragraph, Header }
+
+    /// <summary>
+    /// Element's allignmen in the document. 
+    /// </summary>
+    
+
+    /// <summary>
+    /// Represents a piece of formatted text. 
+    /// </summary>
+    class PDFText : Data.HTML.IArticleElement
+    {
+        public String Content { get; set; }
+        public Font Font { get; set; }
+        public ElementType Type { get; set; } // ??
+        public int Allignment { get; set; }
+
+        /// <summary>
+        /// The default font is Times New Roman, 14pt.
+        /// </summary>
+        public static Font DefaultFont 
+        { 
+            get
+            {
+                Font defaultFont = new Font(Font.FontFamily.TIMES_ROMAN, 14);
+                return defaultFont;
+            }
+        }
+
+        public PDFText(HtmlNode node, ElementType type)
+        {
+            this.Content = node.InnerText;
+            this.Font = DefaultFont;
+            this.Type = type;
+        }
+
+        public PDFText(HtmlNode node, ElementType type, Font font)
+        {
+            this.Content = node.InnerText;
+            this.Font = new Font(font);
+            this.Type = type;
+        }
+
+        /// <summary>
+        /// Adds a phrase created from this element to the target document.
+        /// </summary>
+        /// <param name="pdf"> A document to add to.</param>
+        public void addToPdf(Document pdf)
+        {
+            if(!pdf.IsOpen())
+            {
+                pdf.Open();
+            }
+            Paragraph p = new Paragraph(this.Content, this.Font);
+             pdf.Add(p);
+             if (this.Type == ElementType.Phrase)
+             {
+                 pdf.Add(new Phrase(this.Content, this.Font));
+             }
+
+             else if(this.Type == ElementType.Paragraph)
+             {
+                 pdf.Add(new Paragraph(this.Content, this.Font));
+             }
+             else if(this.Type == ElementType.Header)
+             {
+                 pdf.Add(new Paragraph(this.Content, new Font(this.Font.Family, this.Font.Size + 3)));
+             }
+            
+        }
+    }
+}
+
+
