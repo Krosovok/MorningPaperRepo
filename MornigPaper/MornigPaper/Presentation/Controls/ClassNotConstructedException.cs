5282cbca16ccac7a0bc9bd8b68129c381da4a899
pi14.tsuvariev.viacheslav <slavatsuvarev@gmail.com>
2016-05-19 22:51:59 +0300

    Plus Exceptions

---
+++ MornigPaper/MornigPaper/Presentation/Controls/ClassNotConstructedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MornigPaper.Presentation.Controls
+{
+    class ClassNotConstructedException : Exception
+    {
+        public ClassNotConstructedException() : base() { }
+    }
+}
